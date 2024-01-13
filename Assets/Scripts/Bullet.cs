706d68ba67e0948fc24b5d405303c6ef470b5854
CattoSpot <coconutcannon24@gmail.com>
2024-01-12 18:33:58 -0800

    bullets die now

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float bulletLife;
+    void Start()
+    {
+        StartCoroutine(BulletFade());
+    }
+
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Destroy(gameObject);
+    }
+
+    IEnumerator BulletFade()
+    {
+        yield return new WaitForSeconds(bulletLife);
         Destroy(gameObject);
     }
 }
