d7d1c7d6243ab49be8e440ba6e8b305ee3b8e12a
Con <134752009+Concon99@users.noreply.github.com>
2024-01-27 20:21:58 -1000

    boss is done
    kind of sucks but who cares

--- Assets/Scripts/Boss attacks/Boss 2 attacks/B2Attack3.cs
+++ Assets/Scripts/Boss attacks/Boss 2 attacks/B2Attack3.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 public class B2Attack3 : MonoBehaviour
 {
     [SerializeField] private B2Attack1 _B2Attack1;
+    [SerializeField] private B2Attack4 _B2Attack4;
     public Animator transation;
 
 
     public void Attack3()
     {
             transation.SetBool("playershow", true);
-            _B2Attack1.speed -= 1f;
+            _B2Attack1.speed += 2f;
+            _B2Attack4.speed += 2f;
             StartCoroutine(WaitForAnimationCooldown());
     }
 
@@ -20,5 +22,7 @@ public class B2Attack3 : MonoBehaviour
         // Wait for 0.5 seconds
         yield return new WaitForSeconds(1f);
         transation.SetBool("playershow", false);
+
+
     }
 }
